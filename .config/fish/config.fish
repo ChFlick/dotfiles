d4b1ebdbf0866ea0049c81a1171ad7c687b57401
Christoph Flick <ChFlick@users.noreply.github.com>
2021-03-31 14:35:23 +0200

    Add fzf and bat + configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,3 +14,20 @@ nvm use default --silent
 
 set -gx VISUAL vim
 
+
+# fzf
+####
+set -g FZF_DEFAULT_OPTS '--height 50% --layout=reverse --border --inline-info --preview "bat --color \"always\" {}"'
+set -g FZF_DEFAULT_COMMAND 'fd . "$dir" --type f -L -E ~/Games -E .git -E node_modules '
+set -g FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND 
+set -g FZF_ALT_C_COMMAND 'fd . "$dir" --type d -E ~/Games -E .git -E node_modules'
+set -g FZF_ALT_C_OPTS '--preview "tree -C {} | head -100"'
+
+function _fzf_compgen_path
+  fd --hidden --follow --exclude ".git" . "$1"
+end
+
+# Use fd to generate the list for directory completion
+function _fzf_compgen_dir
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+end
